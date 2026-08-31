256c61ed7cacc448a166cd8d346b57428f5e1a2e
Ezze1996 <luzdelunaambar@gmail.com>
2026-08-30 22:47:52 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,6 @@
-const CACHE_NAME = 'sat-acceso-v2';
 const urlsToCache = [
-  '/SECURITYSAT-Garibaldi/',
-  '/SECURITYSAT-Garibaldi/index.html',
-  '/SECURITYSAT-Garibaldi/securitysat-logo.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './securitysat-logo.png'
 ];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => clients.claim())
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    fetch(event.request)
-      .then(response => response)
-      .catch(() => caches.match(event.request))
-  );
-});
